e3a7c305c3b79575f1eea0c50b2108f7862e79bf
IgnacioRV <nacho.rasche@gmail.com>
2016-09-10 18:29:38 +0200

    Bugfix and Heroku deployment

--- server.js
+++ server.js
@@ -2,25 +2,23 @@
 
 var express = require('express');
 var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
+//var mongoose = require('mongoose');
+//var passport = require('passport');
+//var session = require('express-session');
 var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 require('dotenv').load();
-require('./app/config/passport')(passport);
-
+// require('./app/config/passport')(passport);
 // mongoose.connect(process.env.MONGO_URI);
 
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/public/css', express.static(process.cwd() + '/public/css'));
-
 app.use('/common', express.static(process.cwd() + '/app/common'));
-
+/*
 app.use(session({
 	secret: 'secretClementine',
 	resave: false,
@@ -29,7 +27,8 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
-
+*/
+var passport = "";
 routes(app, passport);
 
 var port = process.env.PORT || 8080;
